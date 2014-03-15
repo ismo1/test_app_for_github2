44d410519f6019452c5de735423bfa50d1aa631f
ISMAEL M <moussa.ismo1@gmail.com>
2014-03-15 16:58:09 +0100

    bootstrap ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 CleanCode::Application.routes.draw do
+  get "pages/index"
+  get "pages/blog"
+  get "pages/about"
+  get "pages/contact"
+
+  root :to => "pages#index"
+ 
+  get ':not_found' => redirect('/'), :constraints => { :not_found => /.*/ }
+
 end
